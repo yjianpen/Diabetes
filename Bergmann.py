c9b9cfa8a3da8731f1b863167c0e4c82ba0298ec
yjianpen <yujianpeng4229@gmail.com>
2019-11-03 23:28:42 -0500

    Create Bergmann.py
    An ode simulation of bergman minimum model

---
+++ Bergmann.py
@@ -0,0 +1,31 @@
+def Bergmann(params,deltaT,steps,G,I,X):
+	t=deltaT
+	Gb=params['Gb']['val']
+	n=params['n']['val']
+	h=params['h']['val']
+	gamma=params['gamma']['val']
+	p1,p2,p3=(params['p1']['val'],params['p2']['val'],params['p3']['val'])
+	diffI=-n*I+gamma*(G-h)*t
+	print("diFFFFFF",gamma,G-h,t)
+	diffX=p2*X+p3*I
+	diffG=p1*G-X*G-p1*Gb
+	print("diffI",-n*I,gamma*(G-h)*t,diffI)
+	return diffI,diffG,diffX
+
+params={'gamma':{'val':5.36*10**(-3),'unit':'uU/mgmin^2'},'p1':{'val':-2.96/100,'unit':'1/min'},'p2':{'val':1.86/100,'unit':'1/min'},'p3':{'val':6.51/1000000,'unit':'ml/uUmin^2'},'p4':{'val':0.098,'unit':'1/ml'},'n':{'val':0.142,'unit':'1/min'},'I0':{'val':200,'unit':'uU/mgmin^2'},'h':{'val':90.9,'unit':'mg/dL'},'n':{'val':0.23,'unit':'min-1'},'G0':{'val':298,'unit':'mg/dL'},'I0':{'val':333,'unit':'uU/mL'}}
+params['Gb']={'val':92.6,'unit':'dL'}
+def update(params,deltaT,steps):
+	results=list()
+	G,I,X=(params['G0']['val'],params['I0']['val'],0)
+	results.append([G,I,X])
+	for i in range(1,steps-1):			
+		diffI,diffG,diffX=Bergmann(params,deltaT,i,G,I,X)
+		G+=deltaT*diffG
+		I+=deltaT*diffI
+		X+=deltaT*diffX
+		results.append([G,I,X])
+	return results
+
+print(update(params,1,20))
+
+
