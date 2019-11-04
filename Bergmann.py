fbffd101bfcdaa3e203e9d8ec3e3600a2dd8dafc
yjianpen <yujianpeng4229@gmail.com>
2019-11-04 09:42:22 -0500

    Update Bergmann.py

--- Bergmann.py
+++ Bergmann.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as pyplot
 def Bergmann(params,deltaT,steps,G,I,X):
 	t=deltaT
 	Gb=params['Gb']['val']
@@ -6,11 +7,12 @@ def Bergmann(params,deltaT,steps,G,I,X):
 	gamma=params['gamma']['val']
 	p1,p2,p3=(params['p1']['val'],params['p2']['val'],params['p3']['val'])
 	diffI=-n*I+max(gamma*(G-h)*t,0)
+	print("zero",gamma*(G-h)*t)
 	diffX=p2*X+p3*I
 	diffG=(p1-X)*G-p1*Gb
 	return diffI,diffG,diffX
 
-params={'gamma':{'val':5.36*10**(-3),'unit':'uU/mgmin^2'},'p1':{'val':-2.96/100,'unit':'1/min'},'p2':{'val':-1.86/100,'unit':'1/min'},'p3':{'val':6.51/1000000,'unit':'ml/uUmin^2'},'p4':{'val':0.098,'unit':'1/ml'},'n':{'val':0.142,'unit':'1/min'},'I0':{'val':200,'unit':'uU/mgmin^2'},'h':{'val':90.9,'unit':'mg/dL'},'n':{'val':0.23,'unit':'min-1'},'G0':{'val':298,'unit':'mg/dL'},'I0':{'val':333,'unit':'uU/mL'}}
+params={'gamma':{'val':5.36*10**(-3),'unit':'uU/mgmin^2'},'p1':{'val':-2.96/100,'unit':'1/min'},'p2':{'val':-1.86/100,'unit':'1/min'},'p3':{'val':6.51/1000000,'unit':'ml/uUmin^2'},'p4':{'val':0.098,'unit':'1/ml'},'n':{'val':0.142,'unit':'1/min'},'I0':{'val':200,'unit':'uU/mgmin^2'},'h':{'val':90.9,'unit':'mg/dL'},'n':{'val':0.23,'unit':'min-1'},'G0':{'val':298,'unit':'mg/dL'},'I0':{'val':300,'unit':'uU/mL'}}
 params['Gb']={'val':92.6,'unit':'dL'}
 def update(params,deltaT,steps):
 	results=list()
@@ -24,6 +26,16 @@ def update(params,deltaT,steps):
 		results.append([G,I,X])
 	return results
 
-print(update(params,1,200))
+def plot(results,deltaT,steps):
+	g=list()
+	for i in range(len(results)):
+		print(results[i],results[i][0])
+		g.append(results[i][0])
+	t=list(range(0,len(g)))
+	plt=pyplot.plot(t,g)
+	pyplot.show()
+
+results=update(params,1,200)
+plot(results,1,200)
 
 
