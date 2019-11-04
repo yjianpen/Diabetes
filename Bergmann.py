4527506cd01fca4281d23cad431336204333b6e7
yjianpen <yujianpeng4229@gmail.com>
2019-11-04 15:34:09 -0500

    Update Bergmann.py
    Included base level for insulin concentration

--- Bergmann.py
+++ Bergmann.py
@@ -6,13 +6,16 @@ def Bergmann(params,deltaT,steps,G,I,X):
 	h=params['h']['val']
 	gamma=params['gamma']['val']
 	p1,p2,p3=(params['p1']['val'],params['p2']['val'],params['p3']['val'])
-	diffI=-n*I+max(gamma*(G-h)*t,0)
+	diffI=-n*max((I-0.95),0)+max(gamma*(G-h)*t,0)+0.95
 	print("zero",gamma*(G-h)*t)
-	diffX=p2*X+p3*I
+	diffX=p2*X+p3*max((I-0.95),0)
 	diffG=(p1-X)*G-p1*Gb
+	## G will converge at Gb (~90), Gb is the base Glucose level
+	## X will converge at -p3*I/p2
+	## I will converge at 0, which is not very accurate
 	return diffI,diffG,diffX
 
-params={'gamma':{'val':5.36*10**(-3),'unit':'uU/mgmin^2'},'p1':{'val':-2.96/100,'unit':'1/min'},'p2':{'val':-1.86/100,'unit':'1/min'},'p3':{'val':6.51/1000000,'unit':'ml/uUmin^2'},'p4':{'val':0.098,'unit':'1/ml'},'n':{'val':0.142,'unit':'1/min'},'I0':{'val':200,'unit':'uU/mgmin^2'},'h':{'val':90.9,'unit':'mg/dL'},'n':{'val':0.23,'unit':'min-1'},'G0':{'val':298,'unit':'mg/dL'},'I0':{'val':300,'unit':'uU/mL'}}
+params={'gamma':{'val':5.36*10**(-3),'unit':'uU/mgmin^2'},'p1':{'val':-2.96/100,'unit':'1/min'},'p2':{'val':-0.86/100,'unit':'1/min'},'p3':{'val':6.51/1000000,'unit':'ml/uUmin^2'},'p4':{'val':0.098,'unit':'1/ml'},'n':{'val':0.142,'unit':'1/min'},'I0':{'val':0.4,'unit':'uU/mgmin^2'},'h':{'val':90.9,'unit':'mg/dL'},'n':{'val':0.23,'unit':'min-1'},'G0':{'val':298,'unit':'mg/dL'}}
 params['Gb']={'val':92.6,'unit':'dL'}
 def update(params,deltaT,steps):
 	results=list()
