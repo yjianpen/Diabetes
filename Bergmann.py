905a2b340e4aa1418d71165552b243f2af37b1a3
yjianpen <yujianpeng4229@gmail.com>
2019-11-05 16:24:18 -0500

    Update Bergmann.py

--- Bergmann.py
+++ Bergmann.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as pyplot
+
 def Bergmann(params,deltaT,steps,G,I,X):
 	t=deltaT
 	Gb=params['Gb']['val']
@@ -15,7 +16,9 @@ def Bergmann(params,deltaT,steps,G,I,X):
 	diffG=(p1-X)*G-p1*Gb
 	## G will converge at k1Gb, Gb is the base Glucose level, k1 and k2 are constants
 	## X will converge at -p3*I/p2
-	## I will converge at k2Ib, which is not very accurate
+	## I will converge at k2Ib
+	## TODO: Add a variable represent 
+	## Ib: basic Insuline level
 	return diffI,diffG,diffX
 
 params={'gamma':{'val':5.36*10**(-3),'unit':'uU/mgmin^2'},'p1':{'val':-2.96/100,'unit':'1/min'},'p2':{'val':-0.86/100,'unit':'1/min'},'p3':{'val':6.51/1000000,'unit':'ml/uUmin^2'},'p4':{'val':0.098,'unit':'1/ml'},'n':{'val':0.142,'unit':'1/min'},'I0':{'val':25,'unit':'uU/ml'},'h':{'val':90.9,'unit':'mg/dL'},'n':{'val':0.23,'unit':'min-1'},'G0':{'val':126,'unit':'mg/dL'}}
@@ -30,9 +33,11 @@ def update(params,deltaT,steps):
 		G+=deltaT*diffG
 		I+=deltaT*diffI
 		## Diet event, suppose the patient takes a diet after 200 mins
-		## Delta function should be a better choice than +=60
+		## TODO: Dirac Delta function should be a better choice than +=60
+		'''
 		if i==int(steps/2):
 			G+=60
+		'''
 		X+=deltaT*diffX
 		results.append([G,I,X])
 	return results
@@ -50,6 +55,6 @@ def plot(results,deltaT,steps,mode='Glucose'):
 	pyplot.show()
 
 results=update(params,1,400)
-plot(results,1,200,mode='Glucose')
+plot(results,1,200,mode='Insulin')
 
 
