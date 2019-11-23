216b55aed6fb742d6309e5a736591ef77a0e8c59
yjianpen <yujianpeng4229@gmail.com>
2019-11-23 17:48:22 -0500

    Update 1st_amputation.py

--- 1st_amputation.py
+++ 1st_amputation.py
@@ -13,23 +13,21 @@ def first_amputation_vector(age,hbA1c,female,ATFIB,HDL,heart_rate,SBP,WBC,MIC_AL
 		vector+=1.010
 	if Stroke_Hist:
 		vector+=1.299
-	print("vector",vector)
 	return np.exp(vector)*diabetes_years**(2.067)
 def first_amputation(age1,age2,hbA1c,female,ATFIB,HDL,heart_rate,SBP,WBC,MIC_ALB,PVD,Stroke_Hist,diabetes_years):
 	v1=first_amputation_vector(age1,hbA1c,female,ATFIB,HDL,heart_rate,SBP,WBC,MIC_ALB,PVD,Stroke_Hist)
 	v2=first_amputation_vector(age2,hbA1c,female,ATFIB,HDL,heart_rate,SBP,WBC,MIC_ALB,PVD,Stroke_Hist)
-	print("v1","v2",v1,v2)
 	return 1-np.exp(v1-v2)
 age1=70
 age2=71
 hbA1c=6
-female=True
+female=False
 ATFIB=False
 HDL=60
 heart_rate=80
 SBP=100
 WBC=8
-diabetes_years=8
+diabetes_years=5
 MIC_ALB,PVD,Stroke_Hist=(False,False,False)
 print(first_amputation(age1,age2,hbA1c,female,ATFIB,HDL,heart_rate,SBP,WBC,MIC_ALB,PVD,Stroke_Hist,diabetes_years))
 
