c186c0a1bb032fa431433b8af3e17c9e8b9766d1
yjianpen <yujianpeng4229@gmail.com>
2019-11-23 16:05:02 -0500

    Update blindness.py

--- blindness.py
+++ blindness.py
@@ -1,16 +1,30 @@
 import numpy as np
+from numpy.random import normal
 def Blindness_vector(age,hbA1c,heart_rate,SBP,WBC,CHF_HIST,IHD_HIST,diabetes_years):
 	vector=-11.607+age*0.047+hbA1c*0.171+heart_rate/10*0.080+0.068*SBP/10+WBC*0.052
 	if CHF_HIST:
 		vector+=0.841
 	if IHD_HIST:
 		vector+=0.610
+	'''
+	se_vector=Blindness_SE(age,hbA1c,heart_rate,SBP,WBC,CHF_HIST,IHD_HIST,diabetes_years)
+	vector+=se_vector*normal(0,0.1,1)
+	print("se_vector",normal(0,0.1,1),vector)
+	'''
 	return np.exp(vector)*diabetes_years
 def Blindness(age1,age2,hbA1c,heart_rate,SBP,WBC,CHF_HIST,IHD_HIST,diabetes_years):
 	v1=Blindness_vector(age1,hbA1c,heart_rate,SBP,WBC,CHF_HIST,IHD_HIST,diabetes_years)
 	v2=Blindness_vector(age2,hbA1c,heart_rate,SBP,WBC,CHF_HIST,IHD_HIST,diabetes_years+age2-age1)
 	print("v1","v2",v1,v2)
 	return 1-np.exp(v1-v2)
+
+def Blindness_SE(age,hbA1c,heart_rate,SBP,WBC,CHF_HIST,IHD_HIST,diabetes_years):
+	vector=0.759+age*0.009+hbA1c*0.032+heart_rate/10*0.039+0.032*SBP/10+WBC*0.019
+	if CHF_HIST:
+		vector+=0.287
+	if IHD_HIST:
+		vector+=0.208
+	return vector
 age1=70 ##age of the patient unit: year 
 age2=71 ##age of the patient unit: year 
 hbA1c=6 ##Hemoglobin A1cv unit:%
