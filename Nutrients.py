131f064627b52045a90829d7fd7f853b249a49c6
yjianpen <yujianpeng4229@gmail.com>
2019-11-23 13:18:21 -0500

    Update Nutrients.py

--- Nutrients.py
+++ Nutrients.py
@@ -4,9 +4,9 @@ foods_list = client.list_foods(5)
 for _ in range(5):
 	food_item = next(foods_list)
 	print(food_item.name)
-foods_search = client.search_foods('cabbage',1)
-coffee=next(foods_search)
-print(coffee)
-report = client.get_food_report(coffee.id)
+foods_search = client.search_foods('Chicken breast',1)
+food=next(foods_search)
+print(food)
+report = client.get_food_report(food.id)
 for nutrient in report.nutrients:
 	print(nutrient.name,nutrient.value,nutrient.unit)
